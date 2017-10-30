79f35664f8333f0c0adda32ede29cbc788f14cf6
lukaskellerstein <lukaskellerstein@gmail.com>
2017-10-30 18:19:34 +0100

    test commit

--- Development.ps1
+++ Development.ps1
@@ -6,7 +6,7 @@ docker-compose -f docker-compose.yml up --build
 
 # Mosquitto - MQTT
 docker build -t cellar.hub.mqtt .
-docker run -d -p 1883:1883 -t cellar.hub.mqtt
+docker run -d -p 1883:1883 -p 9001:9001 -t cellar.hub.mqtt
 
 # RethinkDB
 docker build -t cellar.hub.rethinkdb .
@@ -16,12 +16,12 @@ docker run -d -p 28015:28015 -p 29015:29015 -p 8080:8080 -t cellar.hub.rethinkdb
 docker build -t cellar.hub.mongodb .
 docker run -d -p 27017:27017 -t cellar.hub.mongodb
 
+
 # Workflow
 docker build -t cellar.hub.workflow .
 docker run -it cellar.hub.workflow
 
-# DataFlow
-$Env:ASPNETCORE_ENVIRONMENT = "Development"
-dotnet restore
-dotnet build 
-dotnet run
+#git undo changes
+git fetch --all
+git reset --hard origin/master
+git pull origin master
