096913cd9f34f98ea2d003486183cc77465029a2
lukaskellerstein <lukaskellerstein@gmail.com>
2017-11-10 16:06:27 +0100

    add new workflow - cancel meeting

--- Cellar.Hub.Workflow/cmd/cancelmeeting/workflow.go
+++ Cellar.Hub.Workflow/cmd/cancelmeeting/workflow.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/cellarstone/Cellar.Hub/Cellar.Hub.Workflow/src/abstraction"
+	"github.com/cellarstone/Cellar.Hub/Cellar.Hub.Workflow/src/check"
+	"github.com/cellarstone/Cellar.Hub/Cellar.Hub.Workflow/src/decision"
 	"github.com/cellarstone/Cellar.Hub/Cellar.Hub.Workflow/src/workflow"
 
 	"gopkg.in/mgo.v2/bson"
@@ -13,6 +15,19 @@ func RunWorkflow(name string) {
 	go func() {
 		wf := workflow.NewWorkflow(name)
 
+		// CHANNELS ------------------
+
+		ch1out := make(chan string)
+		ch2out := make(chan string)
+		ch3out := make(chan string)
+		ch4out := make(chan string)
+		ch5out := make(chan string)
+		ch6out := make(chan string)
+		ch7out := make(chan string)
+
+		// PIPELINE ------------------
+
+		//Check if room have some meeting in this time
 		wf.AddTask(&check.CheckActualMeetingTask{
 			BaseTask: abstraction.BaseTask{
 				Type:            "CheckActualMeetingTask",
@@ -20,14 +35,20 @@ func RunWorkflow(name string) {
 				State:           "new",
 				ID:              bson.NewObjectId(),
 				InChannelIndex:  0,
-				OutChannelIndex: 99,
+				OutChannelIndex: 1,
 				InChannel:       workflowIn,
-				OutChannel:      workflowOut,
+				OutChannel:      ch1out,
 			},
-			Senzor:        senzor,
-			Topic:         measurement,
-			PrometheusUrl: gatewayUrl,
+			RoomID: "????",
+			ApiUrl: "http://cellar.hub.api/checkactualmeeting",
 		})
+
+		//decision task
+		chas := make([]chan string, 2)
+		chas[0] = workflowOut
+		chas[1] = ch2out
+		dt := decision.NewTrueFalseDecisionTask("IsThereAnyRunningMeeting", 1, []int{99, 2}, ch1out, chas)
+		wf.AddTask(dt)
 
 		wf.Run()
 	}()
---
+++ Cellar.Hub.Workflow/src/decision/truefalsedecisiontask.go
@@ -0,0 +1,55 @@
+package decision
+
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+//*********************************************
+// TRUE & FALSE Decision Task
+//*********************************************
+type TrueFalseDecisionTask struct {
+	ID               bson.ObjectId `json:"_id" bson:"_id,omitempty"`
+	Name             string        `json:"name" bson:"name"`
+	State            string        `json:"state" bson:"state"`
+	Type             string        `json:"type" bson:"type"`
+	InChannelIndex   int           `json:"inchannelindex" bson:"inchannelindex"`
+	OutChannelsIndex []int         `json:"outchannelsindex" bson:"outchannelsindex"`
+	InChannel        chan string   `json:"-" bson:"-"`
+	OutChannels      []chan string `json:"-" bson:"-"`
+}
+
+func NewTrueFalseDecisionTask(name string, inchannelindex int, outchannelsindex []int, inchannel chan string, outChannels []chan string) *TrueFalseDecisionTask {
+	return &TrueFalseDecisionTask{
+		Name:             name,
+		State:            "new",
+		Type:             "TrueFalseDecisionTask",
+		InChannelIndex:   inchannelindex,
+		OutChannelsIndex: outchannelsindex,
+		InChannel:        inchannel,
+		OutChannels:      outChannels,
+	}
+}
+
+// Execute implement Task.Execute.
+func (t *TrueFalseDecisionTask) Execute() error {
+	t.State = "inprogress"
+
+	for value := range t.InChannel {
+
+		if value == "false" {
+			t.OutChannels[0] <- value
+		} else if value == "true" {
+			t.OutChannels[1] <- value
+		}
+
+	}
+
+	//SEM SE TO NIKDY NEDOSTANE !!!
+	t.State = "completed"
+	return nil
+}
+
+func (t *TrueFalseDecisionTask) ExecuteParallel(value string) error {
+	//nothing here
+	return nil
+}
