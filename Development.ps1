76b76edeb1823ddfe4a48753aec2fe452a7e6f3a
Kellerstein Lukas <lukas.kellerstein@cellarstone.eu>
2017-12-09 19:16:25 +0100

    changes for working right on windows

--- Development.ps1
+++ Development.ps1
@@ -20,6 +20,7 @@ docker run -it cellar.hub.workflow
 
 
 # docker-compose variants
-docker-compose -f docker-compose.windows.yml up --build
 docker-compose -f docker-compose.workflow.windows.yml up --build
-docker-compose -f docker-compose.production.yml up --build
+
+# DEV
+docker-compose -f docker-compose.full.development.windows.yml up --build
