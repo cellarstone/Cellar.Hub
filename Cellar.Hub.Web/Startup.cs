4fbd18e4b5f04406e3aaac07052c41635a46e402
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-01-04 14:58:21 +0100

    some changes

--- Cellar.Hub.Web/Startup.cs
+++ Cellar.Hub.Web/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
-// using Cellar.Hub.Core;
 
 namespace Cellar.Hub.Web
 {
@@ -30,68 +29,15 @@ namespace Cellar.Hub.Web
 
         public void ConfigureServices(IServiceCollection services)
         {
-
-            // services.AddScoped<CellarHubDbContext,CellarHubDbContext>();
-
-
-            // Add framework services.
-            services.AddMvc()
-                .AddJsonOptions(options => {
-                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-                }); ;
+            services.AddMvcCore();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
 
-            
-            // var angularRoutes = new[] {
-            //     "/app/",
-            //     "/hck/",
-            //     "/catalog/"
-            // };
-
-
-
-            // app.Use(async (context, next) =>
-            // {
-            //     if (context.Request.Path.HasValue && null != angularRoutes.FirstOrDefault(
-            //         (ar) => context.Request.Path.Value.StartsWith(ar, StringComparison.OrdinalIgnoreCase)))
-            //     {
-            //         context.Request.Path = new PathString("/");
-            //     }
-
-            //     await next();
-            // });
-
-
-
-
-
-
-
-
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-
-
-                // when the user types in a link handled by client side routing to the address bar 
-                // or refreshes the page, that triggers the server routing. The server should pass 
-                // that onto the client, so Angular can handle the route
-                routes.MapRoute(
-                    name: "spa-fallback",
-                    template: "{*url}",
-                    defaults: new { controller = "Home", action = "Index" }
-                );
-            });
         }
     }
 }
