38e5690230b1812db2abe9957431e533e4cfe0b2
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-01-06 15:15:22 +0100

    add meetings

---
+++ Cellar.Hub.Meetings/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Cellar.Hub.Meetings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "Cellar Hub Meetings";
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:44404")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+        }
+    }
+}
