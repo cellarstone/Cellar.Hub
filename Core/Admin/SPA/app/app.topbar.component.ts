903df53acf6806939d281a23f234e714f80e3658
L3MONADE <tom.lemanh2@gmail.com>
2018-04-29 19:08:14 +0200

    fix issue #5

--- Core/Admin/SPA/app/app.topbar.component.ts
+++ Core/Admin/SPA/app/app.topbar.component.ts
@@ -23,17 +23,17 @@ import {AppComponent} from './app.component';
                 <ul class="topbar-items fadeInDown animated" [ngClass]="{'topbar-items-visible': app.topbarMenuActive}">
                     <li #profile class="profile-item" [ngClass]="{'active-top-menu':app.activeTopbarItem === profile}">
                         <a href="#" (click)="app.onTopbarItemClick($event,profile)">
-                            <img class="profile-image" src="assets/layout/images/avatar.png">
+                            <img class="profile-image" src="assets/layout/images/avatar-new.png">
                             <span class="topbar-item-name">Jane Williams</span>
                         </a>
 
                         <ul class="poseidon-menu fadeInDown">
-                            <li role="menuitem">
+                           <li role="menuitem">
                                 <a href="#">
                                     <i class="fa fa-fw fa-user"></i>
                                     <span>Profile</span>
                                 </a>
-                            </li>
+                            </li> 
                             <li role="menuitem">
                                 <a (click)="app.auth.logout()">
                                     <i class="fa fa-fw fa-sign-out"></i>
