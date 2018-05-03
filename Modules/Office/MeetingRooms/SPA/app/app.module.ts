7b5a520e94728362c010137465d63c7a645b8b04
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-05-03 16:38:27 +0200

    testing DNS hall

--- Modules/Office/MeetingRooms/SPA/app/app.module.ts
+++ Modules/Office/MeetingRooms/SPA/app/app.module.ts
@@ -146,15 +146,13 @@ export class AppModule {
   constructor(apollo: Apollo, httpLink: HttpLink) {
     apollo.create({
       link: httpLink.create({
-        // uri: 'http://localhost:44513/graphql'
-        uri: 'http://127.0.0.1:44513/graphql'
+        uri: 'http://officeapi.cellarstone.hub/graphql'
       }),
       cache: new InMemoryCache()
     }, "office");
     
     apollo.create({
       link: httpLink.create({
-        // uri: 'http://13649ba8.ngrok.io/api/graphql'
         uri: 'http://7e8685a0.ngrok.io/api/graphql'
       }),
       cache: new InMemoryCache()
