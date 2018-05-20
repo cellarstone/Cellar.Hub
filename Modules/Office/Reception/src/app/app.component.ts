472d658bdf324b2847abd7e6359d3c307608c920
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-05-20 19:37:52 +0200

    add automatic update feature to Reception

--- Modules/Office/Reception/src/app/app.component.ts
+++ Modules/Office/Reception/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  
+  constructor(private swUpdate: SwUpdate){}
+
+  ngOnInit(){
+    //if service worker is enabled
+      if(this.swUpdate.isEnabled){
+        //set automatically interval to check a new version
+        interval(60000).subscribe(() => {
+          this.swUpdate.checkForUpdate();
+        });
+        //refresh browser if user agreed
+        this.swUpdate.available.subscribe((event) => {
+          if(confirm("New version available. Reload App ? :-)")){
+            window.location.reload();
+          }
+        });
+      }
+  }
 }
