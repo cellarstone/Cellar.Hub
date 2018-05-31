7f12f3d6932e9405b74326d8773f3e71f02cc2d7
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-05-31 19:26:41 +0200

    change url to k2exchange

--- Modules/Office/MeetingRooms/src/environments/environment.prod.ts
+++ Modules/Office/MeetingRooms/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   officeApiUrl: "http://officeapi.cellarstone.hub",
-  k2exchangeUrl: "http://05c4c996.ngrok.io/api"
+  k2exchangeUrl: "http://930048c9.ngrok.io/api"
 };
