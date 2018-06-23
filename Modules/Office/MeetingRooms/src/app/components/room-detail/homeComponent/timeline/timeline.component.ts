57eac46dc84663987dfec07e1e3cd2fa067701af
Pavel Konecny <pavelkonecny87@gmail.com>
2018-06-23 11:39:10 +0200

    timeline options to global variable

--- Modules/Office/MeetingRooms/src/app/components/room-detail/homeComponent/timeline/timeline.component.ts
+++ Modules/Office/MeetingRooms/src/app/components/room-detail/homeComponent/timeline/timeline.component.ts
@@ -24,7 +24,7 @@ export class TimelineComponent implements OnInit, OnDestroy {
   rebuildTimeline: Observable<number>;
 
   private unsubscribe$ = new Subject();
-
+  private options;
 
   ngOnInit() {
 
@@ -47,25 +47,13 @@ export class TimelineComponent implements OnInit, OnDestroy {
     const items = new vis.DataSet(dsitems);
 
     // Configuration for the Timeline
-    const options = {
+    this.options = {
       height: '200px',
-      // moment: function(date) {
-      //   return vis.moment(date).local('cs');
-      // },
       showCurrentTime: true,
-      // moment: function(date) {
-      //   return vis.moment(date).utcOffset('+02:00');
-      // }
       start: moment().add(-1, 'hours').toDate(),
       end: moment().add(6, 'hours').toDate(),
       horizontalScroll: true,
       zoomKey: 'ctrlKey',
-      // rollingMode: {
-      //   follow: true,
-      //   offset: 0.5
-      // }
-      // min: moment().add(-15,'minutes'),
-      // max: moment().add(2,'hours')
       timeAxis: {
         scale: 'hour',
         step: 1
@@ -74,8 +62,7 @@ export class TimelineComponent implements OnInit, OnDestroy {
     };
 
     // Create a Timeline
-    this.timeline = new vis.Timeline(container, items, options);
-    // timeline.moveTo(moment());
+    this.timeline = new vis.Timeline(container, items, this.options);
     this.rebuildTimeline = Observable.interval(60000);
     this.rebuildTimeline
       .takeUntil(this.unsubscribe$)
@@ -85,41 +72,15 @@ export class TimelineComponent implements OnInit, OnDestroy {
 
   }
 
-
   ngOnDestroy(): void {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
   }
 
-
   resetTimeline() {
     console.log('resetTimeline');
-    const options = {
-      height: '200px',
-      // moment: function(date) {
-      //   return vis.moment(date).utc();
-      // },
-      showCurrentTime: true,
-      // moment: function(date) {
-      //   return vis.moment(date).utcOffset('+02:00');
-      // }
-      start: moment().add(-1, 'hours').toDate(),
-      end: moment().add(6, 'hours').toDate(),
-      horizontalScroll: true,
-      zoomKey: 'ctrlKey',
-      // rollingMode: {
-      //   follow: true,
-      //   offset: 0.5
-      // }
-      // min: moment().add(-15,'minutes'),
-      // max: moment().add(2,'hours')
-      timeAxis: {
-        scale: 'hour',
-        step: 1
-      }
-    };
 
-    this.timeline.setOptions(options);
+    this.timeline.setOptions(this.options);
   }
 
 }
