f414efad10e0b2553d226aa46f8bf71739bdb96f
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-06-27 18:38:06 +0200

    change url back to localhost for development mode in Admin

--- Core/Admin/src/environments/environment.ts
+++ Core/Admin/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     production: false,
     https: false,
     fileServerUrl: "http://localhost:44404",
-    iotUrl: "https://iot.cellarstone.hub",
+    iotUrl: "http://localhost:44403",
     officeApiUrl: "http://localhost:44513",
     workflowUrl: "http://localhost:44405",
     callbackUrl: "http://localhost:44402/callback"
